6d3d14f89a0fd36e0320f1d5eaf3634907678950
stanislav <xiiiovart@gmail.com>
2024-01-12 20:52:15 +0300

    Wednesday update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import styles from './App.module.scss';
 import { Footer } from './components/Footer/Footer';
-import { Header } from './components/Header/Header';
+import { Header, WednesdayHeader } from './components/Header/Header';
 
 function App() {
+  const [day] = useState(new Date().getDay());
+  
   return (
     <div className={styles.app}>
-      <Header />
+      {day === 3 && <WednesdayHeader />}
+      {day !== 3 && <Header />}
       <Footer />
     </div>
   );
